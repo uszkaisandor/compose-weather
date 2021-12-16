d2ad043288b0088b0ddfe436224073857aa0373b
Sándor Uszkai <uszkaisanyi@gmail.com>
2021-12-16 22:49:13 +0100

    Add some basic UI improvements to CurrentWeather component

--- app/src/main/java/com/uszkaisandor/weathercompose/features/current_weather/view/CurrentWeather.kt
+++ app/src/main/java/com/uszkaisandor/weathercompose/features/current_weather/view/CurrentWeather.kt
@@ -1,14 +1,15 @@
 package com.uszkaisandor.weathercompose.features.current_weather.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.uszkaisandor.weathercompose.api.response.WeatherResponse
@@ -16,9 +17,12 @@ import kotlin.math.roundToInt
 
 @Composable
 fun CurrentWeather(weather: WeatherResponse) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .wrapContentHeight()) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(20.dp)
+    ) {
         Image(
             painter = rememberImagePainter(
                 data = "http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png",
@@ -26,14 +30,32 @@ fun CurrentWeather(weather: WeatherResponse) {
                     crossfade(true)
                 }
             ),
+            colorFilter = ColorFilter.tint(
+                Color.White,
+                BlendMode.SrcAtop
+            ),
             contentDescription = null,
             modifier = Modifier
                 .size(100.dp)
                 .align(Alignment.CenterHorizontally)
         )
         val textModifier = Modifier.align(Alignment.CenterHorizontally)
-        Text(text = weather.name, modifier = textModifier)
-        Text(text = weather.weather[0].description, modifier = textModifier)
-        Text(text = weather.main.temp.roundToInt().toString(), modifier = textModifier)
+        Text(
+            // todo refactor it later
+            text = "${weather.main.temp.roundToInt()} °C",
+            style = MaterialTheme.typography.h2,
+            modifier = textModifier,
+            color = Color.White
+        )
+        Text(
+            text = weather.name,
+            modifier = textModifier.padding(top = 12.dp),
+            color = Color.White
+        )
+        Text(
+            text = weather.weather[0].description,
+            textModifier.padding(top = 12.dp),
+            color = Color.White
+        )
     }
 }
