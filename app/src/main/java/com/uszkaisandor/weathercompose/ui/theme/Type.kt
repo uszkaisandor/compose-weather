c09f360ef4428237c545cdddbbecd900ef016671
Sándor Uszkai <uszkaisanyi@gmail.com>
2021-12-16 22:48:47 +0100

    Add roboto font

--- app/src/main/java/com/uszkaisandor/weathercompose/ui/theme/Type.kt
+++ app/src/main/java/com/uszkaisandor/weathercompose/ui/theme/Type.kt
@@ -2,9 +2,27 @@ package com.uszkaisandor.weathercompose.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.uszkaisandor.weathercompose.R
+
+val fontFamily = FontFamily(
+    Font(R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(R.font.roboto_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.roboto_black, weight = FontWeight.Black),
+    Font(R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(R.font.roboto_light, weight = FontWeight.Light),
+    Font(R.font.roboto_thin, weight = FontWeight.Thin),
+    Font(R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(R.font.roboto_blackitalic, weight = FontWeight.Black, style = FontStyle.Italic),
+    Font(R.font.roboto_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.roboto_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.roboto_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.roboto_thin_italic, weight = FontWeight.Thin, style = FontStyle.Italic),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -12,7 +30,13 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
     )
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
